32e94bb4762fbafc748b7575b8ef8e24d4238475
kapop-git <onlyxiad@gmail.com>
2013-11-09 10:46:48 +0800

    面试题10源码

---
+++ 10.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int count(int x){
+    int cnt=0;
+    int tt=x;
+    while(tt){
+        tt = (tt-1) & tt;
+        cnt++;
+    }
+    return cnt;
+}
+
+int main()
+{
+    int a,b;
+    while (cin >> a){
+        cout << count(a) << endl;
+    }
+    return 0;
+}
+
